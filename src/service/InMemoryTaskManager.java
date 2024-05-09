7c8b8ec1aecd3bf312d4f609912bcdb6269c9d74
МÐKatrenko Vladislav <v.katrenko@vardex.ru>
2024-05-09 15:27:40 +0300

    Удалить не нужный импорт

--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -7,7 +7,6 @@ import model.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
