443083c4c1b93d2b944880bc7b3f7013e2ced93a
МÐKatrenko Vladislav <v.katrenko@vardex.ru>
2024-05-25 00:41:01 +0300

    Реализовать метод по преобразованию задачи в строку

---
+++ src/converter/TaskConverter.java
@@ -0,0 +1,10 @@
+package converter;
+
+import model.Task;
+
+public class TaskConverter {
+    public static String toString(Task task) {
+        return task.getId() + "," + task.getTypeTask() + "," + task.getTitle() + "," + task.getStatus() +
+                "," + task.getDescription() + "," + task.getEpicId();
+    }
+}
